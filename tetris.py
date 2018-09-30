a746a227dd15987bd3c48363cfbecdf1e22edbfb
Isaac Avram <avrisaac555@gmail.com>
2018-09-30 16:41:31 +0000

    Added tetris.py

---
+++ tetris.py
@@ -0,0 +1,27 @@
+from scene import *
+import sound
+import random
+import math
+A = Action
+
+class MyScene (Scene):
+	def setup(self):
+		pass
+	
+	def did_change_size(self):
+		pass
+	
+	def update(self):
+		pass
+	
+	def touch_began(self, touch):
+		pass
+	
+	def touch_moved(self, touch):
+		pass
+	
+	def touch_ended(self, touch):
+		pass
+
+if __name__ == '__main__':
+	run(MyScene(), show_fps=False)
