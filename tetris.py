8190be4184ae579382de5a35eb6a60267bd07a63
Isaac Avram <avrisaac555@gmail.com>
2018-10-01 05:57:42 +0000

    Added PieceControl

--- tetris.py
+++ tetris.py
@@ -70,26 +70,53 @@ class Tile(SpriteNode):
 		self.position = pos
 		
 
+class PieceControl ():
+	"""
+	An object that controls a group of tiles.
+	"""
+	def __init__(self, tiles=[]):
+		"""
+		Constructs a new PieceControl.
+		
+		Parameters:
+			tiles: A list of Tile objects under the control of this object.
+		"""
+		self.tiles = tiles
+
+	def reset(self, tiles=[]):
+		self.tiles = tiles
+	
+	def rotate(self):
+		pass
+	
+	def move(self, up=0, right=0, down=0, left=0):
+		pass
+
 class TetrisGame(Scene):
 	"""
 	The main game code for Tetris
 	"""
-	def setup(self):
-		self.background_color = COLORS["bg"]
-		
-		# Root node for all game elements
-		self.game_field = Node(parent=self, position=GRID_POS)
-		
-		# Add the background grid
-		self.bg_grid = build_background_grid()
-		self.game_field.add_child(self.bg_grid)
-		
+	def setup_ui(self):
 		# Root node for UI elements
 		self.ui_root = Node(parent=self)
 		
 		self.left_btn = SpriteNode(**UI["LEFT_BTN"], parent=self.ui_root)
 		
 		self.right_btn = SpriteNode(**UI["RIGHT_BTN"],parent=self.ui_root)
+	
+	def setup(self):
+		self.background_color = COLORS["bg"]
+		
+		# Root node for all game elements
+		self.game_field = Node(parent=self, position=GRID_POS)
+		
+		self.control = PieceControl()
+		
+		# Add the background grid
+		self.bg_grid = build_background_grid()
+		self.game_field.add_child(self.bg_grid)
+		
+		self.setup_ui()
 	
 	def did_change_size(self):
 		pass
